a9ec45929e9fce94022639bcb2cb59d6b3a57414
mrnguyenlk-byte <mrnguyenlk@gmail.com>
2026-08-07 07:10:27 +0700

    Add official BTrading channel links

---
+++ lib/brand-links.ts
@@ -0,0 +1,26 @@
+/** Official BTrading links. Keep this file as the single source of truth. */
+export const BTRADING_LINKS = {
+  website: "https://btrading.org",
+  facebook: "https://www.facebook.com/BTradingOfficial",
+  youtube: "https://www.youtube.com/@btradingmkt",
+  telegram: "https://t.me/btradingdaily",
+  zalo: "https://zalo.me/922222889",
+  tiktok: "https://www.tiktok.com/@bbbnvgvp",
+  phone: "tel:+84922222889",
+} as const
+
+/** Registration forms keep the acquisition source for reporting. */
+export const BTRADING_SIGNUP_LINKS = {
+  youtube:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=YouTube",
+  tiktok:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=TikTok",
+  telegram:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=Telegram",
+  facebook:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=Facebook",
+  zalo:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=Zalo",
+  other:
+    "https://docs.google.com/forms/d/e/1FAIpQLScS8Qn7ZQI1_0_MnPrWUI2uv4QzGUDIB7CPTYoWT4HijsEG1Q/viewform?usp=pp_url&entry.1620932897=Kh%C3%A1c",
+} as const
